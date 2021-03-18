41e09ebdbeec8d095e7adda3b1556a0223314c23
Anghel941 <80909584+Anghel941@users.noreply.github.com>
2021-03-18 14:15:06 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let slider =document.querySelector(".slider_contenedor")
+let sliderIndividual =document.querySelectorAll(".contenido_slider")
+let contador=1;
+let width= sliderIndividual[0].clientWidth;
+let intervalo = 8000;
+
+window.addEventListener("resize",function(){
+    width= sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+     slides();
+},intervalo);
+
+function slides(){
+    slider.style.transform= "translate("+(-(width)*contador)+"px)";
+    slider.style.transition= "transform .8s";
+    contador++;
+
+    if(contador==sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform= "translate(0px)";
+            slider.style.transition= "transform 0s";
+            contador=1;
+        },1500)
+    }
+}
