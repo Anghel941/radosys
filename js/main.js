6425a8344b111e0ffbc9cbd80456fc03215b626c
Anghel941 <80909584+Anghel941@users.noreply.github.com>
2021-03-27 22:14:50 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 let slider =document.querySelector(".slider_contenedor")
 let sliderIndividual =document.querySelectorAll(".contenido_slider")
 let contador=1;
@@ -25,3 +26,34 @@ function slides(){
         },1500)
     }
 }
+
+
+/*bara de menu oculto*/
+
+let ubicacion_principal=window.pageYOffset;
+
+window.addEventListener("scroll",function(){
+    let desplazamiento_actual=window.pageYOffset;
+    if(ubicacion_principal>=desplazamiento_actual){
+        document.getElementsByTagName("nav")[0].style.top ="0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top ="-100px"
+    }
+    ubicacion_principal=desplazamiento_actual;
+})
+/* menu */
+
+let enlaces_menu= document.querySelectorAll(".menu")[0];
+let semaforo=true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
+    if (semaforo) {
+        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
+        semaforo=false;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color="#000";
+        semaforo=true;
+    }
+    enlaces_menu.classList.toggle("menu-oculto")
+})
+
